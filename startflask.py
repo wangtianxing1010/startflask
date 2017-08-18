8501c3bf899a4fa3fc3ba9ed673460848ba5b178
wangtianxing1010 <16573740+wangtianxing1010@users.noreply.github.com>
2017-08-17 21:37:17 -0700

    add flash message effect
    1. link stylesheet in html header
    2. debug startflask.py

--- startflask.py
+++ startflask.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template,flash
+from flask import Flask, render_template, flash
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='secret key'
 
 
-
-app = Flask(__name__)
-
 @app.route('/')
-def index():
-	flash( "")
+def index(methods=['GET','POST']):
+	flash("Welcome my friend")
 	return render_template('index.html')
 	
 	
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/me/Documents/mystuff/myproject/venv"
+set -gx VIRTUAL_ENV "/Users/me/Documents/mystuff/myproject/startflask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
