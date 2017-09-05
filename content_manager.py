66a07d2831c9e719cbdb0855a40e8dc6584ace59
wangtianxing1010 <16573740+wangtianxing1010@users.noreply.github.com>
2017-09-04 20:20:32 -0700

    Some enhancement on the site
    1. Added dynamic dashboard
    2. Added content_manager.py
    3. Added errorhandler 404
    4. updated .gitignore: .DS_Store

---
+++ content_manager.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def Content():
+	TOPIC_DICT = { 'Home':[['1','/urlgoeshere/'],
+							['2','/urlgoeshere/'],
+							['3','/urlgoeshere/']],
+	'Profile':[['1','/urlgoeshere/'],
+				['2','/urlgoeshere/'],
+				['3','/urlgoeshere/']],
+	'Messages':[['1','/urlgoeshere/'],
+				['2','/urlgoeshere/'],
+				['3','/urlgoeshere/']],
+	'Settings':[['1','/urlgoeshere/'],
+				['2','/urlgoeshere/'],
+				['3','/urlgoeshere/']]
+	}
+	
+	return TOPIC_DICT
--- startflask.py
+++ startflask.py
@@ -1,18 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, flash
+from content_manager import Content
 
+TOPIC_DICT = Content() 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='secret key'
 
 
 @app.route('/')
-def index(methods=['GET','POST']):
+def Index(methods=['GET','POST']):
 	flash("Welcome my friend")
 	return render_template('index.html')
 	
 	
+@app.route('/dashboard/')
+def Dashboard(methods=['GET','POST']):
+	flash("Welcome to dashboard")
+	return render_template('dashboard.html', TOPIC_DICT=TOPIC_DICT)
+	
+	
+#Error Handling	
+@app.errorhandler(404)
+def Page_Not_Found(e):
+	flash("Page not found!!")
+	return render_template('404.html')
+
 if __name__ == '__main__':
-	app.run()
+	app.run(debug = True)
 	
